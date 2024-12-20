04bbeddc3fe16c951737aaca848a2fd5f5b71c1d
Chandan K M <chandan.km@tecnotree.com>
2024-12-20 10:24:30 +0530

    DI +IOC+APIs

--- src/main/java/com/firstapp/first_spring_app/controller/SampleController.java
+++ src/main/java/com/firstapp/first_spring_app/controller/SampleController.java
@@ -8,6 +8,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/sample/apis")
 public class SampleController {
 
+
     @GetMapping("/sample1")
     public String sampleApi1(){
         return "This is our first sample API";
